51bfe91f8ebb08ae1f346d1c69b76844d3b03f4b
OdaySabe <odaysabe305@gmail.com>
2022-07-16 13:34:50 +0300

    adding array methods

---
+++ week3/Array Methods/script.js
@@ -0,0 +1,380 @@
+const arr = [12, 13, 12, 123, 123, 111, 12];
+console.log(arr.filter((r) => r % 2 == 0));
+let vegetables = [
+  { name: "Eggplant", color: "purple" },
+  { name: "Carrot", color: "orange" },
+  { name: "Squash", color: "orange" },
+  { name: "Tomatoe", color: "red" },
+  { name: "Onion", color: "white" },
+  { name: "Sweet Potato", color: "orange" },
+];
+console.log(vegetables.filter((v) => v.color === "orange"));
+vegetables.forEach((v) => console.log(v.name, v.color));
+
+let people = [
+  { salary: 1300, goodPerformance: false },
+  { salary: 1500, goodPerformance: true },
+  { salary: 1200, goodPerformance: true },
+  { salary: 1700, goodPerformance: false },
+  { salary: 1600, goodPerformance: true },
+];
+const increment = function (person) {
+  person.salary += 300;
+  return person;
+};
+people.forEach((p) => console.log(increment(p)));
+people.forEach((p) => console.log(p.salary));
+
+let messagesFromDad = [
+  "HI HONEY",
+  "HOW WAS SCHOOL??",
+  "DID YOU EAT TODAY?",
+  "I CAN'T FIND THE REMOTE CONTROL",
+];
+console.log(messagesFromDad.map((p) => p.toLowerCase()));
+console.log("===================");
+let users = [
+  {
+    name: "Leanne Graham",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: { lat: "-37.3159", lng: "81.1496" },
+    },
+  },
+  {
+    name: "Ervin Howell",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: { lat: "-43.9509", lng: "-34.4618" },
+    },
+  },
+];
+console.log(
+  users.map((u) => {
+    return { name: u.name, city: u.address.city };
+  })
+);
+let posts = [
+  {
+    id: 0,
+    text: "I'm not here",
+    comments: [{ id: 0, text: "support that" }],
+  },
+  {
+    id: 1,
+    text: "Find me",
+    comments: [
+      { id: 0, text: "here I am" },
+      { id: 1, text: "rock you like a hurricane" },
+      { id: 2, text: "dum dum" },
+    ],
+  },
+  {
+    id: 2,
+    text: "Where's waldo anyway",
+    comments: [
+      { id: 0, text: "who's waldo" },
+      { id: 1, text: "he's called Effi" },
+    ],
+  },
+  {
+    id: 3,
+    text: "Poof",
+    comments: [{ id: 0, text: "like magic" }],
+  },
+];
+const findCommentByID = (postID, commentID) => {
+  return posts
+    .find((a) => a.id == postID)
+    .comments.find((a) => a.id === commentID);
+};
+console.log("==========");
+console.log(findCommentByID(1, 2));
+let movies = [
+  { title: "Dareangel", studio: "Marvel", year: 2023 },
+  { title: "Batterfly", studio: "Fox", year: 2021 },
+  { title: "League of Ordinary People", studio: "Blizzard", year: 2025 },
+  { title: "Thor: Ragnarok", studio: "Marvel", year: 2017 },
+];
+if (movies.some((a) => a.studio === "Marvel")) {
+  console.log("lets watch a movie");
+  if (movies.every((a) => a.year >= 2020)) {
+    console.log("Futuristic stuff");
+  } else {
+    console.log("Yawn");
+  }
+} else {
+  console.log("Let’s stay home");
+}
+
+/////////////////////////////////
+///////////////Excircises////////
+let data = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618",
+      },
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "Deckow-Crist",
+      catchPhrase: "Proactive didactic contingency",
+      bs: "synergize scalable supply-chains",
+    },
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    address: {
+      street: "Douglas Extension",
+      suite: "Suite 847",
+      city: "McKenziehaven",
+      zipcode: "59590-4157",
+      geo: {
+        lat: "-68.6102",
+        lng: "-47.0653",
+      },
+    },
+    phone: "1-463-123-4447",
+    website: "ramiro.info",
+    company: {
+      name: "Romaguera-Jacobson",
+      catchPhrase: "Face to face bifurcated interface",
+      bs: "e-enable strategic applications",
+    },
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+    address: {
+      street: "Hoeger Mall",
+      suite: "Apt. 692",
+      city: "South Elvis",
+      zipcode: "53919-4257",
+      geo: {
+        lat: "29.4572",
+        lng: "-164.2990",
+      },
+    },
+    phone: "493-170-9623 x156",
+    website: "kale.biz",
+    company: {
+      name: "Robel-Corkery",
+      catchPhrase: "Multi-tiered zero tolerance productivity",
+      bs: "transition cutting-edge web services",
+    },
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+    address: {
+      street: "Skiles Walks",
+      suite: "Suite 351",
+      city: "Roscoeview",
+      zipcode: "33263",
+      geo: {
+        lat: "-31.8129",
+        lng: "62.5342",
+      },
+    },
+    phone: "(254)954-1289",
+    website: "demarco.info",
+    company: {
+      name: "Keebler LLC",
+      catchPhrase: "User-centric fault-tolerant solution",
+      bs: "revolutionize end-to-end systems",
+    },
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+    address: {
+      street: "Norberto Crossing",
+      suite: "Apt. 950",
+      city: "South Christy",
+      zipcode: "23505-1337",
+      geo: {
+        lat: "-71.4197",
+        lng: "71.7478",
+      },
+    },
+    phone: "1-477-935-8478 x6430",
+    website: "ola.org",
+    company: {
+      name: "Considine-Lockman",
+      catchPhrase: "Synchronised bottom-line interface",
+      bs: "e-enable innovative applications",
+    },
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+    address: {
+      street: "Rex Trail",
+      suite: "Suite 280",
+      city: "Howemouth",
+      zipcode: "58804-1099",
+      geo: {
+        lat: "24.8918",
+        lng: "21.8984",
+      },
+    },
+    phone: "210.067.6132",
+    website: "elvis.io",
+    company: {
+      name: "Johns Group",
+      catchPhrase: "Configurable multimedia task-force",
+      bs: "generate enterprise e-tailers",
+    },
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
+    address: {
+      street: "Ellsworth Summit",
+      suite: "Suite 729",
+      city: "Aliyaview",
+      zipcode: "45169",
+      geo: {
+        lat: "-14.3990",
+        lng: "-120.7677",
+      },
+    },
+    phone: "586.493.6943 x140",
+    website: "jacynthe.com",
+    company: {
+      name: "Abernathy Group",
+      catchPhrase: "Implemented secondary concept",
+      bs: "e-enable extensible e-tailers",
+    },
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io",
+    address: {
+      street: "Dayna Park",
+      suite: "Suite 449",
+      city: "Bartholomebury",
+      zipcode: "76495-3109",
+      geo: {
+        lat: "24.6463",
+        lng: "-168.8889",
+      },
+    },
+    phone: "(775)976-6794 x41206",
+    website: "conrad.com",
+    company: {
+      name: "Yost and Sons",
+      catchPhrase: "Switchable contextually-based project",
+      bs: "aggregate real-time technologies",
+    },
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+    address: {
+      street: "Kattie Turnpike",
+      suite: "Suite 198",
+      city: "Lebsackbury",
+      zipcode: "31428-2261",
+      geo: {
+        lat: "-38.2386",
+        lng: "57.2232",
+      },
+    },
+    phone: "024-648-3804",
+    website: "ambrose.net",
+    company: {
+      name: "Hoeger LLC",
+      catchPhrase: "Centralized empowering task-force",
+      bs: "target end-to-end models",
+    },
+  },
+];
+let EX1 = data.map((a) => {
+  return {
+    email: a.email,
+    companyName: a.company.name,
+  };
+});
+console.log(EX1);
+let EX2 = data.filter((a) => a.address.zipcode.startsWith("5"));
+console.log(EX2);
+let EX3 = EX2.map((a) => a.id);
+console.log(EX3);
+let EX4 = data.filter((a) => a.name.startsWith("C")).map((a) => a.name);
+console.log(EX4);
+let EX5 = data.every((a) => a.address.city === "South Christy");
+if (EX5) {
+  console.log("all users are from South Christy ");
+} else {
+  console.log("Not all users from South Christy ");
+}
+let EX6 = data.find((a) => a.address.suite === "Apt. 950").company.name;
+console.log(EX6);
+const pretty = function (user) {
+  console.log(
+    user.name +
+      " lives in " +
+      user.address.city +
+      " and owns a company " +
+      user.company.name
+  );
+};
+data.forEach(pretty);
